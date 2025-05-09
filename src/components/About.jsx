e6e69703910e0c96f44f8b87b3b66e4dec20f930
Pratyush Tyagi <86615560+Pratyush0612@users.noreply.github.com>
2025-05-09 22:33:06 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
         transition={{duration :0.5}}
         className="w-full lg:w-1/2 lg:p-8">
            <div className="flex items-center justify-center">
-            <img className="rounded-2xl" src={profilePic} alt="abt" />
+            
         </div>
             </motion.div> 
           
